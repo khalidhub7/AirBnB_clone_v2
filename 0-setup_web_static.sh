c9e3760f750c9b0c4f2e156836056faea186fa02
khalidhub7 <louguikhalid7@gmail.com>
2024-05-07 18:52:02 -0700

    set up server

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
-# make ur web server works
-dpkg -l | grep nginx > /dev/null 2>&1 || (sudo apt -y update && sudo apt -y upgrade && sudo apt -y install nginx)
-ls /data/web_static/releases/test/ > /dev/null 2>&1 || sudo mkdir -p /data/web_static/releases/test/
-ls /data/web_static/shared/ > /dev/null 2>&1 || sudo mkdir -p /data/web_static/shared/
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install nginx
 
-echo -e "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>" | sudo tee /data/web_static/releases/test/index.html
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p data/web_static/shared/
+sudo touch data/web_static/releases/test/index.html
 
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo echo -e '<!DOCTYPE html>\n<html>\n<body>\n    <h1>Hello, KHaleed!</h1>\n</body>\n</html>' > data/web_static/releases/test/index.html
 
-sudo chown -R ubuntu:ubuntu /data/
+# Remove the symbolic link if it exists
+rm -f /data/web_static/current
+# Create a new symbolic link
+ln -s /data/web_static/releases/test/ /data/web_static/current
+
+chown -R ubuntu:ubuntu data/
 
 sudo sed -i '/server_name _;/a \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 
