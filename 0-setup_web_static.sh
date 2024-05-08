ab37f8ffcd15a0ebad6b613283cd01522c12c847
khalidhub7 <louguikhalid7@gmail.com>
2024-05-07 19:08:23 -0700

    set up server

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,9 +1,13 @@
-#!/usr/bin/env bash
-# Setup web server
-apt update -y
-apt install -y nginx
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
+#!/bin/bash
+set -e
+
+# Update package lists and install Nginx
+sudo apt update -y
+sudo apt install -y nginx
+
+# Create necessary directories and files
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
 echo "<!DOCTYPE html>
 <html>
   <head>
@@ -11,8 +15,21 @@ echo "<!DOCTYPE html>
   <body>
     <p>Hello, KHaleed!</p>
   </body>
-</html>" | tee /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data
-sudo sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
-sudo service nginx restart
+</html>" | sudo tee /data/web_static/releases/test/index.html
+
+# Create symbolic link
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+# Change ownership of directories
+sudo chown -R ubuntu:ubuntu /data/web_static/releases/test/
+sudo chown -R ubuntu:ubuntu /data/web_static/shared/
+
+# Update Nginx configuration
+sudo tee -a /etc/nginx/sites-available/default <<EOF
+    location /hbnb_static {
+        alias /data/web_static/current/;
+    }
+EOF
+
+# Restart Nginx service
+sudo systemctl restart nginx
