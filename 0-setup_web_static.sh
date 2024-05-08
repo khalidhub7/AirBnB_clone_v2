6c885ed5712f703817a4672918500488a9413db2
khalidhub7 <louguikhalid7@gmail.com>
2024-05-07 19:04:30 -0700

    set up server

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,23 +1,18 @@
 #!/usr/bin/env bash
-set -e
-
-sudo apt update -y
-sudo apt upgrade -y
-sudo apt install nginx
-
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
-sudo touch /data/web_static/releases/test/index.html
-
-sudo echo -e '<!DOCTYPE html>\n<html>\n<body>\n    <h1>Hello, KHaleed!</h1>\n</body>\n</html>' > /data/web_static/releases/test/index.html
-
-# Remove the symbolic link if it exists
-rm -f /data/web_static/current
-# Create a new symbolic link
-ln -s /data/web_static/releases/test/ /data/web_static/current
-
-chown -R ubuntu:ubuntu /data/
-
-sudo sed -i '/server_name _;/a \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-
+# Setup web server
+apt update -y
+apt install -y nginx
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    <p>Hello, KHaleed!</p>
+  </body>
+</html>" | tee /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data
+sudo sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
 sudo service nginx restart
