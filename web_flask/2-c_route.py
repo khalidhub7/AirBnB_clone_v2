1af5de63a89a760a3d48660f36871d8f9fcd3e58
khalidhub7 <louguikhalid7@gmail.com>
2024-05-20 02:26:48 -0700

    flask

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,33 +1,26 @@
 #!/usr/bin/python3
-"""run flask server"""
-from markupsafe import escape
-from flask import Flask
-app = Flask(__name__)
+""" web_app """
+from flask import FLASK
+app = FLASK(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def hello_world():
-    """hello returned"""
-    return "Hello HBNB!"
+@app.route('/', strict_slashes=False)
+def hello():
+    return 'Hello HBNB!'
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """hbnb returned"""
-    return "HBNB"
+    return 'HBNB'
 
 
-@app.route("/c/<text>", strict_slashes=False)
-def cIsFun(text):
-    """hbnb returned"""
-    string = ""
+@app.route('/c/<text>', strict_slashes=False)
+def variable(text):
     for i in text:
         if i == '_':
-            string += ' '
-        else:
-            string += i
-    return "C {}".format(string)
+            i = ' '
+    return text
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
