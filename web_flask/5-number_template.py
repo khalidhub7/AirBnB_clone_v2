1ce904d74f8614cabb61c4224376a753c0074a61
khalidhub7 <louguikhalid7@gmail.com>
2024-05-20 03:41:45 -0700

    flask

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,55 +1,60 @@
 #!/usr/bin/python3
-"""run flask server"""
-from flask import Flask, render_template
+""" web_app """
+from flask import Flask, render_template, url_for
+from werkzeug.exceptions import NotFound
 app = Flask(__name__)
 
 
-def makeSpaces(text):
-    string = ""
+@app.route('/', strict_slashes=False)
+def hello():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def variable(text):
+    new = 'C '
     for i in text:
         if i == '_':
-            string += ' '
+            new += ' '
         else:
-            string += i
-    return string
+            new += i
+    return new
 
 
-@app.route("/", strict_slashes=False)
-def hello_world():
-    """hello returned"""
-    return "Hello HBNB!"
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def variable_2(text):
+    new = 'Python '
+    for i in text:
+        if i == '_':
+            new += ' '
+        else:
+            new += i
+    return new
 
 
-@app.route("/hbnb", strict_slashes=False)
-def hbnb():
-    """hbnb returned"""
-    return "HBNB"
+@app.route('/number/<n>', strict_slashes=False)
+def num(n):
+    try:
+        numm = int(n)
+        return '{} is a number'.format(numm)
+    except ValueError:
+        raise NotFound
 
 
-@app.route("/c/<text>", strict_slashes=False)
-def cIsFun(text):
-    """hbnb returned"""
-    return "C {}".format(makeSpaces(text))
+@app.route('/number_template/<n>', strict_slashes=False)
+def num_template(n):
+    try:
+        numm = int(n)
+        return render_template('./templates/5-number.html', numm)
+    except ValueError:
+        raise NotFound
 
 
-@app.route("/python", strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
-def pyIsFun(text="is cool"):
-    """hbnb returned"""
-    return "Python {}".format(makeSpaces(text))
-
-
-@app.route("/number/<int:n>", strict_slashes=False)
-def integer(n):
-    """hbnb returned"""
-    return "{} is a number".format(n)
-
-
-@app.route("/number_template/<int:n>", strict_slashes=False)
-def integer_tmp(n):
-    """hbnb returned"""
-    return render_template('5-number.html', n=n)
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
