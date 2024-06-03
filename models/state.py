5df7e80fd0aa009413178998efc270a7c4dd3aef
khalidhub7 <louguikhalid7@gmail.com>
2024-06-02 19:15:43 -0700

    up

--- models/state.py
+++ models/state.py
@@ -5,7 +5,7 @@ from models.base_model import BaseModel, Base
 from models.city import City
 from os import getenv
 import sqlalchemy
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
@@ -37,3 +37,4 @@ class State(BaseModel, Base):
         def cities(self):
             """getter for list of city instances related to the state"""
             return [city for city in models.storage.all(City).values() if city.state_id == self.id]
+
