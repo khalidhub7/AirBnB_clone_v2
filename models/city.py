dcff29ace5c6859a20d76144c7bfcf35f8eb23de
khalidhub7 <louguikhalid7@gmail.com>
2024-06-10 19:50:58 -0700

    up

--- models/city.py
+++ models/city.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
 
 
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+class City(BaseModel, Base):
+    """ The city class, contains state ID and name
+    Update City for sqlalchemy"""
+    __tablename__ = 'cities'
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+
+    """ the next line mean:
+     city obj can associated with mutiple places 
+     (one-to-many relationship) """
+    places = relationship("Place", backref='cities', cascade='delete')
