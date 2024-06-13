741fd8443d9a36cb3bda675da65774bacf5627bf
khalidhub7 <louguikhalid7@gmail.com>
2024-06-12 21:00:24 -0700

    update console because it shows obj alreedy exists

--- models/amenity.py
+++ models/amenity.py
@@ -1,19 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship
-from sqlalchemy import Column, String
-from models.place import place_amenity
-import os
+from models.base_model import BaseModel
 
 
-class Amenity(BaseModel, Base):
-    __tablename__ = 'amenities'
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        name = Column(String(128), nullable=False)
-        place_amenities = relationship('Place',
-                                       back_populates='amenities',
-                                       secondary=place_amenity
-                                       )
-    else:
-        name = ""
+class Amenity(BaseModel):
+    name = ""
