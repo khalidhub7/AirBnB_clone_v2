94edc05316b60fbdb17d0e0081da499eb23d5a41
khalidhub7 <louguikhalid7@gmail.com>
2024-06-14 03:50:34 -0700

    update user add relationship ##

--- models/user.py
+++ models/user.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 """ user class """
-from os import getenv
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class User(BaseModel, Base):
-    """ user attributes_ """
+    """ user attributes """
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = 'users'
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
-        first_name = Column(String(128))
-        last_name = Column(String(128))
+        first_name = Column(String(128), nullable=False)
+        last_name = Column(String(128), nullable=False)
         places = relationship(
             'Place',
             backref='user',
