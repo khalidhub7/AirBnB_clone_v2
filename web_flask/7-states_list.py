00f68c810c5b9354667afb5095235c1e694897ac
khalidhub7 <louguikhalid7@gmail.com>
2024-06-23 06:00:11 -0700

    Setting up first route states🌐

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,15 +5,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     states = sorted(storage.all('State').values(),
                     key=lambda state: state.name)
     return render_template('7-states_list.html', states=states)
 
+
 @app.teardown_appcontext
 def close(current_Session):
     storage.close()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
