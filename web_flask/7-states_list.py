81d262163ca32c92c35bceebff87958d42f75ea7
khalidhub7 <louguikhalid7@gmail.com>
2024-06-23 05:41:26 -0700

    Setting up first route states 🏁🚦🌐

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python3
-"""
-Starts a Flask web applicationi
-"""
-
-from flask import Flask, render_template
-from models import *
+""" flask list all states """
 from models import storage
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """Display a HTML page with the states listed in alphabetical order"""
-    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    states = sorted(storage.all('State').values(),
+                    key=lambda state: state.name)
     return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """Closes the storage on teardown"""
+def close(current_Session):
     storage.close()
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
