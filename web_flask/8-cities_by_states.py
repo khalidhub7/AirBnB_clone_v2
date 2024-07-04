f4cfc3d30bf73c6a43482a0bd160d09acb7bbeba
khalidhub7 <louguikhalid7@gmail.com>
2024-07-04 15:39:02 -0700

    route(cities by state)

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -2,19 +2,21 @@
 """ list cities by states """
 from models import storage
 from models.state import State
-from models.city import City
 from flask import Flask, render_template
 app = Flask(__name__)
+
 
 def citiesBYstates():
     """ retrieve cities by states from the database """
     all_citiesBYstates = storage.all(State).values()
     return all_citiesBYstates
 
+
 @app.route('/cities_by_states', strict_slashes=False)
 def states_list():
     all_states = citiesBYstates()
     return render_template('8-cities_by_states.html', states=all_states)
+
 
 @app.teardown_appcontext
 def teardown(exception):
