efa1eeae4f2cb21a2e4ef2768db4e9f8bdf8def4
khalidhub7 <louguikhalid7@gmail.com>
2024-07-04 12:28:45 -0700

    setting up route states 🌐#5

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,20 +3,22 @@
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-
 app = Flask(__name__)
+
 
 @app.route('/states_list', strict_slashes=False)
 def list_states():
-    """Fetches and renders a list of all states sorted by name."""
+    """ all states """
     all_states = storage.all(State).values()
     sorted_states = sorted(all_states, key=lambda state: state.name)
     return render_template('7-states_list.html', states=sorted_states)
 
+
 @app.teardown_appcontext
 def teardown(exception):
-    """Removes the current SQLAlchemy Session."""
+    """ remove current SQLAlchemy Session """
     storage.close()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
