5e601d4f8c3d13989d8be4459534f0bfdffb4679
khalidhub7 <louguikhalid7@gmail.com>
2024-07-04 19:49:37 -0700

    up

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,10 +5,9 @@ Flask app to list all states sorted by name.
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-
 app = Flask(__name__)
 
-@app.route('/states_list', strict_slashes=False)
+
 def list_states():
     """
     Displays a HTML page with a list of all State objects present in DBStorage,
@@ -16,7 +15,14 @@ def list_states():
     """
     all_states = storage.all(State).values()
     sorted_states = sorted(all_states, key=lambda state: state.name)
-    return render_template('7-states_list.html', states=sorted_states)
+    return sorted_states
+
+
+@app.route('/states_list', strict_slashes=False)
+def all_states():
+    states = list_states()
+    return render_template('7-states_list.html', states=states)
+
 
 @app.teardown_appcontext
 def teardown(exception):
@@ -24,6 +30,7 @@ def teardown(exception):
     Removes the current SQLAlchemy Session after each request.
     """
     storage.close()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
