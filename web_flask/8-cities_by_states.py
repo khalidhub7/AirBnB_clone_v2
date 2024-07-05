2ddeb7d2ebd907265da9a5430359be913cc3d78e
khalidhub7 <louguikhalid7@gmail.com>
2024-07-04 19:33:54 -0700

    route     (cities by state)

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,25 +1,32 @@
-
 #!/usr/bin/python3
-"""
-starts a Flask web applicationstart Flask application"""
-
-from flask import Flask, render_template
-from models import *
+""" list cities by states """
 from models import storage
+from models.state import State
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
+def sorted_states():
+    """ retrieve and sort states and cities by name """
+    all_states = storage.all(State).values()
+    sorted_states = sorted(all_states, key=lambda state: state.name)
+    for state in sorted_states:
+        state.cities = sorted(state.cities, key=lambda city: city.name)
+    return sorted_states
+
+
 @app.route('/cities_by_states', strict_slashes=False)
-def cities_by_states():
-    """display the states and cities listed in alphabetical order"""
-    states = storage.all("State").values()
-    return render_template('8-cities_by_states.html', states=states)
+def states_list():
+    all_citiesBYstates = sorted_states()
+    return render_template('8-cities_by_states.html',
+                           states=all_citiesBYstates)
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """closes the storage on teardown"""
+def teardown(exception):
+    """ remove current SQLAlchemy Session """
     storage.close()
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
