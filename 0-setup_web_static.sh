23848d34fa67a00fce1a7ea3a89d751a226f0472
khalidhub7 <louguikhalid7@gmail.com>
2024-08-16 21:54:07 -0700

    setup web_ser...

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,32 +1,15 @@
 #!/usr/bin/env bash
-# setup servers to deploy files
-
-
-# Installs Nginx if it's not already installed
+# make ur web server works
 dpkg -l | grep nginx > /dev/null 2>&1 || (sudo apt -y update && sudo apt -y upgrade && sudo apt -y install nginx)
-
-#Creates dir if doesn't exist
 ls /data/web_static/releases/test/ > /dev/null 2>&1 || sudo mkdir -p /data/web_static/releases/test/
 ls /data/web_static/shared/ > /dev/null 2>&1 || sudo mkdir -p /data/web_static/shared/
 
-
-sudo tee /data/web_static/releases/test/index.html > /dev/null <<EOF
-<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>
-EOF
+echo -e "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>" | sudo tee /data/web_static/releases/test/index.html
 
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
 sudo chown -R ubuntu:ubuntu /data/
 
-sudo sed -i '23i \
-\tlocation /hbnb_static {\n\
-\t\talias /data/web_static/current/;\n\
-\t}' /etc/nginx/sites-available/default
-
+sudo sed -i '/server_name _;/a \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 
 sudo service nginx restart
