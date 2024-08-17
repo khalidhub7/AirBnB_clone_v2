b7c446a564cf9b0f28556cf823253c25f9ab2d1e
khalidhub7 <louguikhalid7@gmail.com>
2024-08-16 23:17:16 -0700

    rar

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,23 +1,16 @@
 #!/usr/bin/python3
-"""
-Fabric script to genereate tgz archive
-execute: fab -f 1-pack_web_static.py do_pack
-"""
-
+""" fabric file that generate tgz archive """
 from datetime import datetime
-from fabric.api import *
+from fabric.api import local
 
 
 def do_pack():
-    """
-    making an archive on web_static folder
-    """
-
+    """ make archive web_static folder """
     time = datetime.now()
-    archive = 'web_static_' + time.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
-    local('mkdir -p versions')
-    create = local('tar -cvzf versions/{} web_static'.format(archive))
-    if create is not None:
-        return archive
+    archive_name = 'web_static_' + time.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
+    local("mkdir -p versions")
+    rar = local('tar -cvzf versions/{} web_static'.format(archive_name))
+    if rar is not None:
+        return archive_name
     else:
         return None
